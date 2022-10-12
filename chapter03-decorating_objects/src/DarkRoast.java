1b86921c107d0c83faa82aadb751955755db02c1
phamvanthanh <phamvanthanh6093@gmail.com>
2022-10-12 20:34:53 +0700

    chapter03

---
+++ chapter03-decorating_objects/src/DarkRoast.java
@@ -0,0 +1,9 @@
+public class DarkRoast extends Beverage {
+    public DarkRoast(){
+        description = "Most Excellent Dark Roast";
+    }
+
+    public double cost(){
+        return .85;
+    }
+}
